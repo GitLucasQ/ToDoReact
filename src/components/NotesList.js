f102348ee74d4ab31e355d1d2981a4468f5eb09a
LucasQ <lucass2408@hotmail.com>
2020-08-31 17:39:55 -0500

    First commit

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -67,7 +67,6 @@ export default class NotesList extends Component {
                                 </div>
                             </div>
                         </div>
-
                     )
                 }
             </div>
